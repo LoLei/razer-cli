164e86524e691c810e5a5a0d70b6bf071c6d66e5
Lorenz Leitner <lorenz.leitner@student.tugraz.at>
2019-03-30 10:27:04 +0100

    List devices and their effects

--- razer-x-color.py
+++ razer-x-color.py
@@ -65,6 +65,50 @@ def set_color(color):
 
     return r, g, b
 
+def get_effects_of_device(device):
+    # All relevant effects
+    effects = [
+        'breath_random',
+        'breath_single',
+        'breath_dual',
+        'breath_triple',
+        'pulsate',
+        'reactive',
+        'ripple',
+        'ripple_random',
+        'spectrum',
+        'static',
+        'startlight_single',
+        'starlight_dual',
+        'starlight_random',
+        'wave',
+    ]
+
+    return [effect for effect in effects if device.fx.has(effect)]
+
+def list_devices(device_manager):
+    """
+    List all connected Razer devices
+    https://github.com/openrazer/openrazer/blob/master/examples/list_devices.py
+    """
+
+    if args.verbose:
+        print("Found {} Razer devices".format(len(device_manager.devices)))
+
+    # Iterate over each device and pretty out some standard information about each
+    for device in device_manager.devices:
+        print("{}:".format(device.name))
+        print("   type: {}".format(device.type))
+        print("   serial: {}".format(device.serial))
+        print("   firmware version: {}".format(device.firmware_version))
+        print("   driver version: {}".format(device.driver_version))
+
+        device_effects = get_effects_of_device(device)
+        print("   effects: {}".format(device_effects))
+
+        if (args.list_devices_long):
+            print("   capabilities: {}".format(device.capabilities))
+    print()
 
 def main():
     """ Main entry point of the app """
@@ -79,8 +123,8 @@ def main():
     # Create a DeviceManager. This is used to get specific devices
     device_manager = DeviceManager()
 
-    if args.verbose:
-        print("Found {} Razer devices".format(len(device_manager.devices)))
+    if (args.list_devices or args.list_devices_long):
+        list_devices(device_manager)
 
     # Disable daemon effect syncing.
     # Without this, the daemon will try to set the lighting effect to every device.
@@ -130,9 +174,17 @@ if __name__ == "__main__":
                         help="increase output verbosity",
                         action="store_true")
 
-    parser.add_argument('-c','--color', nargs='+',
+    parser.add_argument("-c","--color", nargs="+",
                         help="choose color (default: X color1), use one argument "
                              "for hex, or three for base10 rgb")
+
+    parser.add_argument("-l", "--list_devices",
+                        help="list available devices",
+                        action="store_true")
+
+    parser.add_argument("-ll", "--list_devices_long",
+                        help="list available devices and all their capabilities",
+                        action="store_true")
 
     args = parser.parse_args()
 
