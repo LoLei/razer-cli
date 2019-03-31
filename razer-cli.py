966c0bd955bec14ebac3b90793a25e405ed9c768
Lorenz Leitner <lorenz.leitner@student.tugraz.at>
2019-03-31 11:03:07 +0200

    Handle three values for rgb color

--- razer-cli.py
+++ razer-cli.py
@@ -18,12 +18,19 @@ import util
 import settings
 
 def parse_color_argument(color):
-    # Hex: Just one input argument
-    rgb = color[0]
-    r, g, b = util.hex_to_decimal(rgb)
+    r = 0
+    g = 0
+    b = 0
 
-    # RGB: Three base10 input arguments
-    # TODO
+    if len(color) == 1:
+        # Hex: Just one input argument
+        rgb = color[0]
+        r, g, b = util.hex_to_decimal(rgb)
+    elif len(color) == 3:
+        # RGB: Three base10 input arguments
+        r = int(color[0])
+        g = int(color[1])
+        b = int(color[2])
 
     return r, g, b
 
