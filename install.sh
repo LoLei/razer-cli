c9f6bdb38cdcf16bc59e6c5ebf45ad4301eda5d1
Lorenz Leitner <lorenz.leitner@student.tugraz.at>
2019-09-21 11:48:55 +0200

    Add /usr/bin symlink to make i3 config recognize it

--- install.sh
+++ install.sh
@@ -1,37 +1,59 @@
-#!/bin/sh
+#!/bin/bash
 
 # Adapted from https://github.com/GideonWolfe/Zathura-Pywal/blob/master/install.sh
+
+echo "Started installation..."
 
 # Set a path for local binaries
 # add this to your path
 BINPATH=$HOME/.local/bin
 
-# make sure this path exists
+# Make sure this path exists
 mkdir -p "$BINPATH"
 
-# make the scripts executable
+# Make the scripts executable
 chmod u+x ./razer-cli.py
 
-# link the scripts to the PATH
+# Link the scripts to the PATH
 ln -s "$(pwd)/razer-cli.py" "$BINPATH/razer-cli"
 
 # Add user bin path to local bashrc
 while true; do
-    read -p "Do you want to add $BINPATH to your PATH
-    by appending to your bashrc? [y/n] " yn
-    case $yn in
-        [Yy]* ) echo "Yes";
-          echo export PATH="$BINPATH:\$PATH" >> $HOME/.bashrc;
-          source $HOME/.bashrc
-          break;;
-        [Nn]* ) echo "No";break;;
-        * ) echo "Please answer yes or no.";;
-    esac
+  read -p "Do you want to add $BINPATH to your PATH by appending to your bashrc? [y/n] " yn
+  case $yn in
+    [Yy]* ) echo "Yes";
+            echo export PATH="$BINPATH:\$PATH" >> $HOME/.bashrc;
+            source $HOME/.bashrc;
+            break;;
+    [Nn]* ) echo "No";break;;
+    * ) echo "Please answer yes or no.";;
+  esac
 done
 
+# Symlink to to /usr/bin if wanted
+# Needed to have it recognized via i3 config
+ROOT_SYMLINK=0
+while true; do
+  read -p "Do you want to create a symlink in /usr/bin? (Required for i3 compatibility, requires root) [y/n] " yn
+  case $yn in
+    [Yy]* ) echo "Yes";
+            ROOT_SYMLINK=1
+            break;;
+    [Nn]* ) echo "No";break;;
+    * ) echo "Please answer yes or no.";;
+  esac
+done
+
+if [ $ROOT_SYMLINK == 1 ];
+then
+  if [ $EUID != 0 ];
+  then
+    sudo ln -s "$(pwd)/razer-cli.py" "/usr/bin/razer-cli"
+  else
+    ln -s "$(pwd)/razer-cli.py" "/usr/bin/razer-cli"
+  fi
+fi
+
 cat <<EOM
-Installed to "$BINPATH"
-Make sure "$BINPATH" is in your PATH
-you may do so by adding the following line to your bashrc or zshrc
-export PATH="$BINPATH:\$PATH"
+Installation finished.
 EOM
